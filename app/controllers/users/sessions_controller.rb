a6c66b0b71b8db876aac3fca3b5b8c83fa304160
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-14 15:12:59 +0800

    flash message for user sign in

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,35 @@
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    if self.resource = warden.authenticate!(auth_options)
+      set_flash_message!(:success, :signed_in)
+    else
+      set_flash_message!(:danger, :signed_in)
+    end
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :danger, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cs_module_posts
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   root to: 'static#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
