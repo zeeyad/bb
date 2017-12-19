dd0351258fac92c6e56e25bb6aa8bc37e01ffa83
AMBER CHLOE <zeeyadrahman@gmail.com>
2017-12-19 20:28:32 +0800

    style error for sign in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def active?(path)
   	"active" if current_page?(path)
   end
+
+  def bootstrap_class_for(flash_type)
+    {
+        success: "alert-success",
+        error: "alert-error",
+        alert: "alert-danger",
+        notice: "alert-info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def bootstrap_glyphs_icon(flash_type)
+    {
+        success: "fa-check-circle-o",
+        error: "fa-check-circle-o",
+        alert: "fa-exclamation-triangle",
+        notice: "fa-info-circle"
+    }[flash_type.to_sym] || 'glyphicon-screenshot'
+  end
+
 end
