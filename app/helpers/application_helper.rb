2ecd1d072f82838869b2fc1bde8dd3b58ca7d936
AMBER CHLOE <zeeyadrahman@gmail.com>
2017-12-21 19:10:02 +0800

    fix navigation for active nav links during posts show method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
   	"active" if current_page?(path)
   end
 
+  def active?(path)
+    "active" if current_page?(path)
+  end
+
   def bootstrap_class_for(flash_type)
     {
         success: "alert-success",
