45a685c27ed34d25c0bfb4da625fb046d7151213
AMBER CHLOE <zeeyadrahman@gmail.com>
2018-01-11 05:41:53 +0800

    secret key base

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Bb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.secret_key_base = 'ebaf140470419eceee769002afbead445b0a8ddbb6cc38b0b8dcb13762471e433c35eeb305153b1c4167820d462490644ad5fde85a9197b6794edaf03802957a'
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
