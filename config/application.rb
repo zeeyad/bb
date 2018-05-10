e34553afb094746f21b7615241dab1d1986758af
AMBER CHLOE <zeeyadrahman@gmail.com>
2018-05-11 00:15:36 +0800

    revert

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Bb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.secret_key_base = ENV['SECRET_KEY_BASE']
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
