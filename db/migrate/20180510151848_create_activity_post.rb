e49ee649fe27bbdb825cefeeb1a192f89458bf12
AMBER CHLOE <zeeyadrahman@gmail.com>
2018-05-10 23:42:02 +0800

    added end

--- db/migrate/20180510151848_create_activity_post.rb
+++ db/migrate/20180510151848_create_activity_post.rb
@@ -7,4 +7,5 @@ class CreateActivityPost < ActiveRecord::Migration[5.1]
       add_column :activity_posts, :end_time, :time # Only if using reconfirmable  end
       add_column :activity_posts, :user_id, :integer # Only if using reconfirmable  end
       add_column :activity_posts, :status, :integer # Only if using reconfirmable  end
+  end
 end
